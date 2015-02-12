20eb0c321382ffa0d772e9d26c0f5e79fdce7f94
Dante Elrik <dante@cloudshopper.com>
2015-02-12 11:56:10 -0500

    Adding a state layer to our stack to count number of division operations

--- Main.hs
+++ Main.hs
@@ -4,17 +4,16 @@ module Main where
 
 main :: IO()
 main  = print $ "hello"
-{-
+{-1
 In a pure lang
 exception handling may be mimicked by
 introducting a type to represent
 computations that may raise an exception.
 -}
-
-data M a       = Raise Exception | Return a
 type Exception = String
-
-{-
+data Term      = Con Int
+               | Div Term Term
+{-2
 Notice how our M a is a parametric type.
 it maps over some arbitrary type a -
 the constructors are compatible with mzero.
@@ -27,8 +26,37 @@ computation that *returned* some value of type a.
 
 Eg: M Int
 -}
-data Term = Con Int
-          | Div Term Term
+data M a       = Raise Exception | Return a
+
+{-3
+State
+
+Introduce state by creating a type synonym for M
+data M a = Raise Exception
+         | Return a
+
+here, M has kind * (no unfilled parameters)
+
+Furthemore its a data type, not a constructor.  So we
+can't use it as a constructor until we do this:
+
+type M a = State -> (a, State)
+
+now, we have a constructor which takes
+a parameter of type a (kind *)
+and produces a function that takes a state param.
+
+and reutns a value of kind * which is a pair
+of the enclosed type parameter a, and the state the
+
+computation was initialised with.
+-}
+
+type M a       = State -> (a, State)
+type State     = Int
+-- We will use this to count the number of
+-- Div operations performed.
+
 
 
 runM            :: Show a => M a -> a
